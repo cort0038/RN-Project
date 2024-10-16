5582a83f0610d484f5f5d5716115f00c9b29754d
Luis Cortorreal <cort0038@algonquinlive.com>
2024-10-15 22:09:27 -0400

    Refactor AddPersonScreen component to use TouchableOpacity for save and cancel buttons

--- screens/AddPersonScreen.js
+++ screens/AddPersonScreen.js
@@ -1,56 +1,59 @@
+import {GlobalContext} from "context/GlobalContext"
+
 import React, {useContext, useState} from "react"
 import {
-	Button,
 	Keyboard,
 	KeyboardAvoidingView,
 	Platform,
 	Text,
 	TextInput,
+	TouchableOpacity,
 	TouchableWithoutFeedback,
 	View
 } from "react-native"
 import DatePicker from "react-native-modern-datepicker"
 import {savePerson} from "utils/actions"
 import {styles} from "utils/styles"
-import {GlobalContext} from "context/GlobalContext"
 
 export const AddPersonScreen = ({navigation}) => {
 	const {addPerson} = useContext(GlobalContext)
 	const [name, setName] = useState("")
-	const [dob, setDob] = useState("")
+	const [date, setDate] = useState("")
 
 	return (
-		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-			<KeyboardAvoidingView
-				style={styles.containerAddPerson}
-				behavior={Platform.OS === "ios" ? "padding" : "height"}
-				keyboardVerticalOffset={100}>
-				<Text style={styles.labelAddPerson}>Name</Text>
-				<TextInput
-					style={styles.inputAddPerson}
-					placeholder="Enter name"
-					value={name}
-					onChangeText={setName}
-				/>
-				<Text style={styles.labelAddPerson}>Date of Birth</Text>
-				<DatePicker
-					mode="calendar"
-					onDateChange={setDob}
-					style={styles.datePickerAddPerson}
-				/>
-				<View style={styles.buttonContainerAddPerson}>
-					<Button
-						title="Cancel"
-						onPress={() => navigation.navigate("PeopleScreen")}
-						color="#F44336"
+		<KeyboardAvoidingView
+			style={styles.containerAddPerson}
+			behavior={Platform.OS === "ios" ? "padding" : "height"}>
+			<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+				<View style={styles.viewAddPerson}>
+					<Text style={styles.labelAddPerson}>Name</Text>
+					<TextInput
+						style={styles.inputAddPerson}
+						placeholder="Enter name"
+						value={name}
+						onChangeText={setName}
 					/>
-					<Button
-						title="Save"
-						onPress={() => savePerson(name, dob, addPerson, navigation)}
-						color="#4CAF50"
+
+					<Text style={styles.labelAddPerson}>Date of Birth</Text>
+					<DatePicker
+						mode="calendar"
+						onDateChange={setDate}
+						style={styles.datePickerAddPerson}
 					/>
+
+					<TouchableOpacity
+						style={styles.buttonAddIdea}
+						onPress={() => savePerson(name, date, addPerson, navigation)}>
+						<Text style={styles.buttonTextAddIdea}>Save</Text>
+					</TouchableOpacity>
+
+					<TouchableOpacity
+						style={styles.deleteButtonIdea}
+						onPress={() => navigation.navigate("PeopleScreen")}>
+						<Text style={styles.deleteButtonTextIdea}>Cancel</Text>
+					</TouchableOpacity>
 				</View>
-			</KeyboardAvoidingView>
-		</TouchableWithoutFeedback>
+			</TouchableWithoutFeedback>
+		</KeyboardAvoidingView>
 	)
 }
