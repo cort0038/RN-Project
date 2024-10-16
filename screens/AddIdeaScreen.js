fbcda576814c40235760010a981391b863a3ae01
Luis Cortorreal <cort0038@algonquinlive.com>
2024-10-15 22:09:17 -0400

    Refactor AddIdeaScreen component to use KeyboardAvoidingView and TouchableWithoutFeedback

--- screens/AddIdeaScreen.js
+++ screens/AddIdeaScreen.js
@@ -1,8 +1,18 @@
+import {GlobalContext} from "context/GlobalContext"
 import React, {useContext, useState} from "react"
-import {Image, Text, TextInput, TouchableOpacity, View} from "react-native"
+import {
+	Image,
+	Keyboard,
+	KeyboardAvoidingView,
+	Platform,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	TouchableWithoutFeedback,
+	View
+} from "react-native"
 import {saveIdea, takePicture} from "utils/actions"
 import {styles} from "utils/styles"
-import {GlobalContext} from "context/GlobalContext"
 
 export const AddIdeaScreen = ({route, navigation}) => {
 	const {personId} = route.params
@@ -11,30 +21,39 @@ export const AddIdeaScreen = ({route, navigation}) => {
 	const [image, setImage] = useState(null)
 
 	return (
-		<View style={styles.containerAddIdea}>
-			<Text style={styles.titleAddIdea}>Add a Gift Idea</Text>
-			<TextInput
-				style={styles.inputAddIdea}
-				placeholder="Gift Idea"
-				value={text}
-				onChangeText={setText}
-			/>
-			{image && <Image source={{uri: image}} style={styles.imageAddIdea} />}
-			<TouchableOpacity
-				style={styles.buttonAddIdea}
-				onPress={() => takePicture(setImage)}>
-				<Text style={styles.buttonTextAddIdea}>Take Picture</Text>
-			</TouchableOpacity>
-			<TouchableOpacity
-				style={styles.buttonAddIdea}
-				onPress={() => saveIdea(text, image, personId, addIdea, navigation)}>
-				<Text style={styles.buttonTextAddIdea}>Save</Text>
-			</TouchableOpacity>
-			<TouchableOpacity
-				style={styles.buttonAddIdea}
-				onPress={() => navigation.navigate("IdeaScreen", {personId})}>
-				<Text style={styles.buttonTextAddIdea}>Cancel</Text>
-			</TouchableOpacity>
-		</View>
+		<KeyboardAvoidingView
+			behavior={Platform.OS === "ios" ? "padding" : "height"}
+			style={styles.containerAddIdea}>
+			<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+				<View style={styles.viewAddIdea}>
+					<Text style={styles.titleAddIdea}>Add a Gift Idea</Text>
+					<TextInput
+						style={styles.inputAddIdea}
+						placeholder="Gift Idea"
+						value={text}
+						onChangeText={setText}
+					/>
+					{image && <Image source={{uri: image}} style={styles.imageAddIdea} />}
+
+					<TouchableOpacity
+						style={styles.buttonAddIdea}
+						onPress={() => takePicture(setImage)}>
+						<Text style={styles.buttonTextAddIdea}>Take Picture</Text>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.buttonAddIdea}
+						onPress={() =>
+							saveIdea(text, image, personId, addIdea, navigation)
+						}>
+						<Text style={styles.buttonTextAddIdea}>Save</Text>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.buttonAddIdea}
+						onPress={() => navigation.navigate("IdeaScreen", {personId})}>
+						<Text style={styles.buttonTextAddIdea}>Cancel</Text>
+					</TouchableOpacity>
+				</View>
+			</TouchableWithoutFeedback>
+		</KeyboardAvoidingView>
 	)
 }
